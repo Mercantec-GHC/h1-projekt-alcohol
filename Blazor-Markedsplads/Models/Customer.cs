c3e30b6e3a542f93d2c5184da5fe4c419e2211f5
Tetiana-lab <tania.reshetylova@gmail.com>
2025-05-22 12:22:53 +0200

    added zipcode and city

--- Blazor-Markedsplads/Models/Customer.cs
+++ Blazor-Markedsplads/Models/Customer.cs
@@ -8,7 +8,13 @@ namespace Blazor_Markedsplads.Models
         [Required, StringLength(50)] public string Name { get; set; }
         [Required, EmailAddress] public string Email { get; set; }
         [Range(18, 120)] public int Age { get; set; }
+
         public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public int ZipCode { get; set; }
+
         public int Phone { get; set; }
         public bool IsSeller { get; set; }
         [Required, MinLength(6)] public string Password { get; set; }
